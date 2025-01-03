59ed3676ef803dd4ca40f247efea6d72c9194446
Jace Babin <jbabin91@gmail.com>
2025-01-03 15:40:55 -0600

    add shadcn components
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

---
+++ app/components/ui/separator.tsx
@@ -0,0 +1,29 @@
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils.ts';
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = 'horizontal', decorative = true, ...props },
+    ref,
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      className={cn(
+        'shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+        className,
+      )}
+      decorative={decorative}
+      orientation={orientation}
+      {...props}
+    />
+  ),
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
+
+export { Separator };
